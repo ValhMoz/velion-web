2c09923c3f95568fdd5a78e7500818b419827711
miguel_onvaa <monbass2003@gmail.com>
2024-06-03 22:12:28 +0200

    cambios en la vista de pacientes

--- pages/includes/dashboard-patients.php
+++ pages/includes/dashboard-patients.php
@@ -25,6 +25,7 @@
       color: #000 !important;
     }
 
+
     .navbar-dark .navbar-toggler-icon {
       filter: invert(1);
     }
@@ -60,6 +61,76 @@
       border-color: #222;
       /* Borde del mismo color que el fondo */
     }
+
+    .custom-color {
+      color: #000000;
+    }
+
+    .profile-header {
+      width: 1200px;
+      /* Ajusta el ancho según tus necesidades */
+      height: 300px;
+      /* Ajusta la altura según tus necesidades */
+      background-image: url('../assets/custom/img/foto_fondo_perfil.jpg');
+      background-size: cover;
+      /* Ajusta la imagen para que cubra todo el div */
+      background-position: center;
+      /* Centra la imagen en el div */
+      position: relative;
+      /* Para posicionar el contenido dentro del div */
+      border-radius: 10px;
+    }
+
+    .profile-picture {
+      border: none;
+    }
+
+    .nav-link {
+      color: #343a40;
+      /* Color oscuro para los botones no seleccionados */
+    }
+
+    .nav-link.active {
+      background-color: #343a40 !important;
+      /* Color oscuro */
+      color: white !important;
+      /* Texto blanco para contraste */
+    }
+
+    .nav-link:hover {
+      background-color: #495057;
+      /* Color oscuro más claro en hover */
+      color: white;
+    }
+
+    .link-wrapper {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+      color: inherit;
+      /* Mantiene el color del texto según el contexto */
+    }
+
+    .link-wrapper img {
+      margin-right: 10px;
+      /* Espaciado entre la imagen y el texto */
+    }
+
+    .link-wrapper span {
+      font-size: 1.2em;
+      /* Ajusta el tamaño del texto */
+      color: black;
+      /* Cambia el color del texto a negro */
+      text-decoration: none;
+      /* Elimina el subrayado */
+      font-size: 1.3rem;
+      font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    }
+
+    .link-wrapper:hover span {
+      text-decoration: none;
+      /* Asegura que el texto no se subraye al pasar el mouse */
+    }
   </style>
 </head>
 
@@ -67,9 +138,11 @@
 
   <nav class="navbar sticky-top navbar-expand-lg navbar-dark shadow">
     <div class="container">
-      <img src="../assets/custom/img/foto_perfil.jpg" alt="Foto de Perfil" class="rounded-circle me-2"
-        style="width: 30px; height: 30px;">
-      <a class="navbar-brand" href="#"><?php echo $nombre . ' ' . $apellidos; ?></a>
+      <a href="userdetail.php?usuario_id=<?php echo $DNI ?>" class="link-wrapper">
+        <img src="../assets/custom/img/foto_perfil.jpg" alt="Foto de Perfil" class="rounded-circle"
+          style="width: 30px; height: 30px;">
+        <span><?php echo $nombre . ' ' . $apellidos; ?></span>
+      </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -94,11 +167,6 @@
           <li class="nav-item">
             <a class="nav-link" href="shop.php">
               <i class="bi bi-shop"></i> Tienda
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="userdetail.php?usuario_id=<?php echo $DNI ?>">
-              <i class="bi bi-person"></i> Perfil
             </a>
           </li>
           <li class="nav-item">
--- pages/userdetail.php
+++ pages/userdetail.php
@@ -28,28 +28,28 @@ if (isset($_GET['usuario_id'])) {
     <div class="profile-header">
         <img src="../assets/custom/img/foto_perfil.jpg" alt="Foto de Perfil" class="profile-picture">
     </div>
-    <div class="row">
+    <div class="row" style="margin-right: 5.7%; margin-left: 0.02%; height: 50vh;">
         <div class="col-md-3">
             <div class="profile-info text-center">
-                <h1><? echo ($usuario[0]['nombre'] . ' ' .  $usuario[0]['apellidos']); ?></h1>
-                <b><? echo ($usuario[0]['rol']); ?></b>
+                <h1 class="custom-color"><? echo ($usuario[0]['nombre'] . ' ' .  $usuario[0]['apellidos']); ?></h1>
+                <b class="custom-color"><? echo ($usuario[0]['rol']); ?></b>
             </div>
         </div>
         <div class="col-md-9">
             <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                 <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="pills-about-tab" data-bs-toggle="pill" data-bs-target="#pills-about" type="button" role="tab" aria-controls="pills-about" aria-selected="true">Acerca de</button>
+                    <button class="nav-link active" id="pills-about-tab" data-bs-toggle="pill" data-bs-target="#pills-about" type="button" role="tab" aria-controls="pills-about" aria-selected="true" style="color: #000000">Acerca de</button>
                 </li>
                 <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="pills-appointments-tab" data-bs-toggle="pill" data-bs-target="#pills-appointments" type="button" role="tab" aria-controls="pills-appointments" aria-selected="false">Citas</button>
+                    <button class="nav-link" id="pills-appointments-tab" data-bs-toggle="pill" data-bs-target="#pills-appointments" type="button" role="tab" aria-controls="pills-appointments" aria-selected="false" style="color: #000000">Citas</button>
                 </li>
                 <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="pills-history-tab" data-bs-toggle="pill" data-bs-target="#pills-history" type="button" role="tab" aria-controls="pills-history" aria-selected="false">Historial Médico</button>
+                    <button class="nav-link" id="pills-history-tab" data-bs-toggle="pill" data-bs-target="#pills-history" type="button" role="tab" aria-controls="pills-history" aria-selected="false" style="color: #000000">Historial Médico</button>
                 </li>
                 <?
                 if ($rol == "Paciente") {
                     echo ('
-                    <button class="nav-link" id="pills-data-tab" data-bs-toggle="pill" data-bs-target="#pills-data" type="button" role="tab" aria-controls="pills-data" aria-selected="false">Modificar datos</button>
+                    <button class="nav-link" id="pills-data-tab" data-bs-toggle="pill" data-bs-target="#pills-data" type="button" role="tab" aria-controls="pills-data" aria-selected="false" style="color: #000000">Modificar datos</button>
                     ');
                 }
                 ?>
@@ -57,14 +57,14 @@ if (isset($_GET['usuario_id'])) {
                 if ($rol != "Paciente") {
                     echo ('
                         <li class="nav-item" role="presentation">
-                        <a class="nav-link" href="users.php">Volver atrás</a>
+                        <a class="nav-link" href="users.php" style="color: #000000">Volver atrás</a>
                         </li>');
                 }
                 ?>
             </ul>
-            <div class="tab-content" id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-about" role="tabpanel" aria-labelledby="pills-about-tab">
-                    <div class="content-section">
+            <div class="tab-content custom-color" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-about" role="tabpanel" aria-labelledby="pills-about-tab" >
+                    <div class="content-section" style="margin-right: 10%">
                         <h3>Acerca de</h3>
                         <p><? echo ($usuario[0]['acerca_de']); ?></p>
                     </div>
@@ -72,7 +72,7 @@ if (isset($_GET['usuario_id'])) {
                 <div class="tab-pane fade" id="pills-appointments" role="tabpanel" aria-labelledby="pills-appointments-tab">
                     <div class="content-section">
                         <h3>Histórico de Citas</h3>
-                        <ul class="list-group">
+                        <ul class="list-group" style="margin-right: 10%">
                             <?php foreach ($citas as $cita) : ?>
                                 <li class="list-group-item"><? echo $cita['fecha_hora'] . ' ' .  $cita['descripcion']; ?></li>
                             <?php endforeach; ?>
@@ -82,7 +82,7 @@ if (isset($_GET['usuario_id'])) {
                 <div class="tab-pane fade" id="pills-history" role="tabpanel" aria-labelledby="pills-history-tab">
                     <div class="content-section">
                         <h3>Historial Médico</h3>
-                        <ul class="list-group">
+                        <ul class="list-group" style="margin-right: 10%">
                             <?php foreach ($informes as $informe) : ?>
                                 <li class="list-group-item"><? echo $informe['fecha_hora'] . ' ' .  $informe['historial_descripcion']; ?></li>
                             <?php endforeach; ?>
@@ -92,26 +92,26 @@ if (isset($_GET['usuario_id'])) {
                 <div class="tab-pane fade" id="pills-data" role="tabpanel" aria-labelledby="pills-data-tab">
                     <div class="content-section">
                         <h3>Cambiar datos</h3>
-                        <div class="card">
+                        <div class="card" style="margin-right: 10%">
                             <div class="card-body">
                                 <form action="../scripts/user_manager.php" method="POST">
                                     <input type="hidden" id="action" name="action" value="actualizar_datos">
                                     <input type="hidden" id="usuario_id" name="usuario_id" value="<?php echo $DNI ?>">
 
-                                    <div class="mb-3">
-                                        <label for="email" class="form-label">Correo Electrónico</label>
-                                        <input type="email" class="form-control" id="email" name="email" placeholder="Correo electrónico" value="<?php echo $correo ?>">
+                                    <div class="mb-2">
+                                        <label for="email" class="form-label" style="margin-left: 5px;">Correo Electrónico:</label>
+                                        <input type="email" class="form-control" id="email" name="email" placeholder="Correo electrónico" style="border: 0.1px solid black;" value="<?php echo $correo ?>">
                                     </div>
-                                    <div class="mb-3">
-                                        <label for="pass" class="form-label">Nueva contraseña</label>
-                                        <input type="password" class="form-control" id="pass" name="pass" placeholder="Contraseña" required>
+                                    <div class="mb-2">
+                                        <label for="pass" class="form-label" style="margin-left: 5px;">Nueva contraseña:</label>
+                                        <input type="password" class="form-control" id="pass" name="pass" placeholder="Contraseña" style="border: 0.1px solid black;" required>
                                     </div>
-                                    <div class="mb-3">
-                                        <label for="confirmPassword" class="form-label">Confirmar nueva contraseña</label>
-                                        <input type="password" class="form-control" id="confirmPass" name="confirmPass" placeholder="Confirmar Contraseña" required>
+                                    <div class="mb-2">
+                                        <label for="confirmPassword" class="form-label" style="margin-left: 5px;">Confirmar nueva contraseña:</label>
+                                        <input type="password" class="form-control" id="confirmPass" name="confirmPass" placeholder="Confirmar Contraseña" style="border: 0.1px solid black;" required>
                                         <div id="passwordError" class="text-danger"></div>
                                     </div>
-                                    <button type="submit" class="btn btn-primary" onclick="return validatePassword()">Guardar Cambios</button>
+                                    <button type="submit" class="btn btn-dark" style="border: 0.1px solid black;" onclick="return validatePassword()">Guardar Cambios</button>
                                 </form>
                             </div>
                         </div>
